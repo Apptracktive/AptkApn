b28d34ed1dcdf6837b2f082ed50f5d7a93215e0b
Jérémy BRUN-PICARD <jeremy.brunpicard@apptracktive.com>
2016-09-26 12:12:45 +0200

    Full blank project

---
+++ AptkApn.Sample/AptkApn.Sample.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AptkApn.Sample.Android
+{
+    [Activity(Label = "AptkApn.Sample.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
